a868154661ccf282af0d51c5f9fc131cd55c81f3
shyonmanesha <134707849+shyonmanesha@users.noreply.github.com>
2023-05-26 19:38:12 +0530

    Add files via upload

---
+++ myfirstprograme.py
@@ -0,0 +1,9 @@
+print('hello, World!')
+person=input("What is your name? ")
+print('Hello,',person)
+
+
+
+
+
+
---
+++ mysecondprograme.py
@@ -0,0 +1,13 @@
+print("=================== Print My Age,Weight,Hight=====================")
+
+age=13
+print(age)
+
+weight = 40
+unit = "kg"
+print(str(weight) + unit)
+
+hight = 120
+unit = "cm"
+print(str(hight) + unit)
+
